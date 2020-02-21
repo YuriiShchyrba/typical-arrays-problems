1f6ca7699626f7c67cb27cd14581e6ab314de6ef
Yurii Shchyrba <yurashchyrba@gmail.com>
2020-02-21 01:19:12 -0500

    feat: reaalize functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ( array.length ==0) return 0;
+  return Math.min.apply(null,array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ( array.length ==0) return 0;
+  return Math.max.apply(null,array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( array.length ==0) return 0;
+  var sum = array.reduce((acc,val)=>{return acc+val;},0);
+  return sum/array.length;
 }
